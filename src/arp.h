0277822ec67d9c5c96c49561ee42e73279ba8dee
Yatsu <107571716+DevYatsu@users.noreply.github.com>
2025-07-09 17:32:31 +0200

    arp protocol base comments for future implementation add, need to implement IPV4 first

---
+++ src/arp.h
@@ -0,0 +1,13 @@
+
+// According to the rfc, the ARP aims to convert Network Protocol Addresses
+// to 48.bit Ethernet Addresses for Transmission on Ethernet Hardware
+//
+// Goal: Implement the method of Converting IP adresses to
+// Ethernet addresses
+
+#ifndef ARP_H
+#define ARP_H
+
+
+#endif
+ 
